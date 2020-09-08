de6a9a1a1339035a0982c24d941433a771e5d014
Aniket Kumar Verma <vermaaniket213@gmail.com>
2020-09-09 00:39:06 +0530

    Updates

---
+++ precache-manifest.cbaf5ca5c27e15784d87629d0c84f4f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8049dc44fa2bd13e8f49363e3b08a45f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "2ec756c90a2fab8d5182",
+    "url": "/resume/static/css/2.8ce86be7.chunk.css"
+  },
+  {
+    "revision": "27ea1cfd63bdcc6e42e2",
+    "url": "/resume/static/css/main.f4558637.chunk.css"
+  },
+  {
+    "revision": "2ec756c90a2fab8d5182",
+    "url": "/resume/static/js/2.45c68916.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/resume/static/js/2.45c68916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ea1cfd63bdcc6e42e2",
+    "url": "/resume/static/js/main.0c8fc99e.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "1f669089ed39c85024142de53c5b8253",
+    "url": "/resume/static/media/Resume_2.1f669089.jpg"
+  }
+]);
